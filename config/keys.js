203c3dd85d95bbd4e83b1cb1360d35ec9ca6fc9c
olazaida <alosha.sugar@hotmail.com>
2019-11-13 15:04:57 +0200

    first att

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports ={
-    mongoURI : "mongodb://yara:yara@cluster0-shard-00-00-dmhrc.mongodb.net:27017,cluster0-shard-00-01-dmhrc.mongodb.net:27017,cluster0-shard-00-02-dmhrc.mongodb.net:27017/First-Project?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+    mongoURI : "mongodb+srv://olaz:1234@cluster0-y7b0e.mongodb.net/test?retryWrites=true&w=majority",
     port: process.env.PORT
 }
